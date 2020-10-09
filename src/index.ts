8277df6f74f32af4a5037df485d934d0078153a2
Wingman J. Lee <42913823+ryuash@users.noreply.github.com>
2020-10-09 09:55:26 +0800

    Feature/data block (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import ExampleComponent from './example';
 import ButtonExample from './button_example';
+import DataBlock from './data_block';
 
 export {
-  ExampleComponent, ButtonExample,
+  ButtonExample,
+  DataBlock,
 };
