51a5b71c77af40fb698f3e70103f880bf7514ea6
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2020-11-10 11:42:13 +0800

    cleaned code

--- src/statbilities/index.test.tsx
+++ src/statbilities/index.test.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Example from '.';
+import Statbilities from '.';
 
-describe('Example', () => {
+describe('Statbilities', () => {
   it('Works with base props', () => {
-    expect(Example).toBeTruthy();
+    expect(Statbilities).toBeTruthy();
     const args = {
       data: [{
-        name: '1', value: 184136.334,
+        name: 'bonded', value: 184136.334,
       },
       {
-        name: '2', value: 7099.13997,
+        name: 'unbonded', value: 7099.13997,
       }],
       info: {
         price: 2.75, inflation: 7.01, marketCap: 515551195.53, communityPool: 370536.4747,
       },
     };
     const wrap = mount(
-      <Example
+      <Statbilities
         {...args}
       />,
     );
     expect(wrap).not.toBeNull();
     expect(wrap.find('h1').first().text()).toBe('Statbilities');
     expect(wrap.find('h4').first().text()).toBe('0.19 m');
+    expect(wrap.find('h4').last().text()).toBe('Community Pool');
   });
 });
