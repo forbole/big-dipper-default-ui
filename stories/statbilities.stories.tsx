711d65f0e081f1508c369ebfacd3924579cb27ab
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2020-11-10 11:36:10 +0800

    fixed naming

--- stories/statbilities.stories.tsx
+++ stories/statbilities.stories.tsx
@@ -17,10 +17,10 @@ export const Default = Template.bind({
 
 Default.args = {
   data: [{
-    name: '1', value: 184136.334,
+    name: 'bonded', value: 184136.334,
   },
   {
-    name: '2', value: 7099.13997,
+    name: 'unbonded', value: 7099.13997,
   }],
   info: {
     price: 2.75, inflation: 7.01, marketCap: 515551195.53, communityPool: 370536.4747,
