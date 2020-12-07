201e52760d0c09c08e8636727dee83748e846af2
Wingman <ryuash123@gmail.com>
2020-12-07 15:48:52 +0800

    dixed: mobile mdx

--- stories/utils/validators_list.tsx
+++ stories/utils/validators_list.tsx
@@ -13,163 +13,160 @@ export const labelsList = {
   status: 'Status',
 };
 
-export const dummyDataInactive = [
-  {
-    moniker: {
-      rawValue: 'Forbole',
-      imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-      displayName: 'Forbsdsfgkbhfskhfsdhole',
-      display: <AvatarDisplay
-        imageUrl="https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg"
-        alt="avatar image"
-        title="Forbole"
-      />,
-    },
-    operatorAddress: '31afsdff2',
-    self: {
-      rawValue: 10,
-      display: '10%',
-    },
-    votingPower: {
-      rawValue: 1000,
-      display: '110',
-      percentDisplay: '106%',
-      visual: <DataBlockCustom />,
-    },
-    status: {
-      rawValue: 'active',
-      className: 'active',
-      display: 'active',
-    },
-  },
-  {
-    moniker: {
-      rawValue: 'Forbole',
-      imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-      displayName: 'Forbsdsfgkbhfskhfsdhole',
-      display: <AvatarDisplay
-        imageUrl="https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg"
-        alt="avatar image"
-        title="Forbole"
-      />,
-    },
-    operatorAddress: '311342',
-    self: {
-      rawValue: 10,
-      display: '10%',
-    },
-    votingPower: {
-      rawValue: 1000,
-      display: '110,000',
-      percentDisplay: '10%',
-      visual: <DataBlockCustom />,
-    },
-    status: {
-      rawValue: 'active',
-      className: 'active',
-      display: 'active',
-    },
-  },
-];
+export const getInactiveDummy = (device:string) => {
+  const display = device === 'mobile' ? 'Forbole' : (
+    <AvatarDisplay
+      imageUrl="https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg"
+      alt="avatar image"
+      title="Forbole"
+    />
+  );
+  return (
+    [
+      {
+        moniker: {
+          rawValue: 'Forbole',
+          imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
+          display,
+        },
+        operatorAddress: '31afsdff2',
+        self: {
+          rawValue: 10,
+          display: '10%',
+        },
+        votingPower: {
+          rawValue: 1000,
+          display: '110',
+          percentDisplay: '106%',
+          visual: <DataBlockCustom />,
+        },
+        status: {
+          rawValue: 'active',
+          className: 'active',
+          display: 'active',
+        },
+      },
+      {
+        moniker: {
+          rawValue: 'Forbole',
+          imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
+          display,
+        },
+        operatorAddress: '311342',
+        self: {
+          rawValue: 10,
+          display: '10%',
+        },
+        votingPower: {
+          rawValue: 1000,
+          display: '110,000',
+          percentDisplay: '10%',
+          visual: <DataBlockCustom />,
+        },
+        status: {
+          rawValue: 'active',
+          className: 'active',
+          display: 'active',
+        },
+      },
+    ]
+  );
+};
 
-export const dummyData = [
-  {
-    moniker: {
-      rawValue: 'Forbole',
-      imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-      displayName: 'Forbsdsfgkbhfskhfsdhole',
-      display: <AvatarDisplay
-        imageUrl="https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg"
-        alt="avatar image"
-        title="Forbole"
-      />,
-    },
-    operatorAddress: '3s12',
-    commission: {
-      rawValue: 90,
-      display: '90%',
-    },
-    self: {
-      rawValue: 11,
-      display: '11%',
-    },
-    votingPower: {
-      rawValue: 10,
-      display: '10',
-      percentDisplay: '45%',
-      visual: <DataBlockCustom />,
-    },
-    status: {
-      rawValue: 'active',
-      className: 'active',
-      display: 'active',
-    },
-  },
-  {
-    moniker: {
-      rawValue: 'Forbole1',
-      imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-      displayName: 'Forbsdsfgkbhfskhfsdhole',
-      display: <AvatarDisplay
-        imageUrl="https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg"
-        alt="avatar image"
-        title="Forbole1"
-      />,
-    },
-    operatorAddress: '312',
-    commission: {
-      rawValue: 10,
-      display: '10%',
-    },
-    self: {
-      rawValue: 10,
-      display: '10%',
-    },
-    votingPower: {
-      rawValue: 55,
-      display: '55',
-      percentDisplay: '5%',
-      visual: <DataBlockCustom />,
-    },
-    status: {
-      rawValue: 'active',
-      className: 'active',
-      display: 'active',
-    },
-  },
-  {
-    moniker: {
-      rawValue: 'Forbole2',
-      imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-      displayName: 'Forbsdsfgkbhfskhfsdhole',
-      display: <AvatarDisplay
-        imageUrl="https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg"
-        alt="avatar image"
-        title="Forbole2"
-      />,
-    },
-    operatorAddress: '31loo2',
-    commission: {
-      rawValue: 100,
-      display: '100%',
-    },
-    self: {
-      rawValue: 9,
-      display: '9%',
-    },
-    votingPower: {
-      rawValue: 1000,
-      display: '110,000',
-      percentDisplay: '10%',
-      visual: <DataBlockCustom />,
-    },
-    status: {
-      rawValue: 'active',
-      className: 'active',
-      display: 'active',
-    },
-  },
-];
+export const getActiveDummy = (device:string) => {
+  const display = device === 'mobile' ? 'Forbole' : (
+    <AvatarDisplay
+      imageUrl="https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg"
+      alt="avatar image"
+      title="Forbole"
+    />
+  );
+  return (
+    [
+      {
+        moniker: {
+          rawValue: 'Forbole',
+          imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
+          display,
+        },
+        operatorAddress: '3s12',
+        commission: {
+          rawValue: 90,
+          display: '90%',
+        },
+        self: {
+          rawValue: 11,
+          display: '11%',
+        },
+        votingPower: {
+          rawValue: 10,
+          display: '10',
+          percentDisplay: '45%',
+          visual: <DataBlockCustom />,
+        },
+        status: {
+          rawValue: 'active',
+          className: 'active',
+          display: 'active',
+        },
+      },
+      {
+        moniker: {
+          rawValue: 'Forbole1',
+          imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
+          display,
+        },
+        operatorAddress: '312',
+        commission: {
+          rawValue: 10,
+          display: '10%',
+        },
+        self: {
+          rawValue: 10,
+          display: '10%',
+        },
+        votingPower: {
+          rawValue: 55,
+          display: '55',
+          percentDisplay: '5%',
+          visual: <DataBlockCustom />,
+        },
+        status: {
+          rawValue: 'active',
+          className: 'active',
+          display: 'active',
+        },
+      },
+      {
+        moniker: {
+          rawValue: 'Forbole2',
+          imageUrl: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
+          display,
+        },
+        operatorAddress: '31loo2',
+        commission: {
+          rawValue: 100,
+          display: '100%',
+        },
+        self: {
+          rawValue: 9,
+          display: '9%',
+        },
+        votingPower: {
+          rawValue: 1000,
+          display: '110,000',
+          percentDisplay: '10%',
+          visual: <DataBlockCustom />,
+        },
+        status: {
+          rawValue: 'active',
+          className: 'active',
+          display: 'active',
+        },
+      },
+    ]
+  );
+};
 
 /**
  * Helper hook that deals with tab changing in mui Tabs
