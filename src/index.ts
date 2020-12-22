61c69ccc9b537c4ccdb847aba7fec473ce858c72
ryuash <ryuash123@gmail.com>
2020-12-22 10:50:37 +0800

    wip: i need food?

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ import ValidatorInfo from './components/validator_info';
 import ActivityDetails from './components/activity_details';
 import InfoPopover from './components/info_popover';
 import PowerEvents from './components/power_events';
+import ValidatorBlocks from './components/validator_blocks';
 
 export {
   DataBlock,
@@ -42,4 +43,5 @@ export {
   ActivityDetails,
   InfoPopover,
   PowerEvents,
+  ValidatorBlocks,
 };
